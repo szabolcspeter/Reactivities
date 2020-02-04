76e129a96aea3547adef66dca8511d3eb41d0d39
Szabolcs Péter <szabolcs.peter@medicover.hu>
2020-02-04 10:07:17 +0100

    End of section 14.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -30,15 +32,17 @@ namespace Application.Activities
                 RuleFor(x => x.Category).NotEmpty();
                 RuleFor(x => x.Date).NotEmpty();
                 RuleFor(x => x.City).NotEmpty();
-                RuleFor(x => x.Venue).NotEmpty();   
+                RuleFor(x => x.Venue).NotEmpty();
             }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
@@ -52,10 +56,24 @@ namespace Application.Activities
                     Category = request.Category,
                     Date = request.Date,
                     City = request.City,
-                    Venue = request.Venue 
+                    Venue = request.Venue
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
